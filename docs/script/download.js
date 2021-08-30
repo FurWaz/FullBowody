c02e7fb5f65ee317281d90bc61c1b4ba3b946a4b
FurWaz <fur.waz06@gmail.com>
2021-08-30 14:25:31 +0200

    added get started content

--- docs/script/download.js
+++ docs/script/download.js
@@ -4,11 +4,11 @@ function loadDownload() {
     CONTAINER.appendChild(createText("FullBowody's application binaries", null, "subtitle"));
     CONTAINER.appendChild(
         createElement("div", null, "align-x obj-container", null, null, 
-            createElement("div", null, "align-y item-container", null, dl_linux,
+            createElement("div", "dl_linux", "align-y item-container", null, dl_linux,
                 createIcon("./resources/linux.png", null, "item-icon"),
                 createText("Linux", null, "item-text")
             ),
-            createElement("div", null, "align-y item-container", null, dl_windows,
+            createElement("div", "dl_windows", "align-y item-container", null, dl_windows,
                 createIcon("./resources/windows.png", null, "item-icon"),
                 createText("Windows", null, "item-text")
             )
@@ -18,17 +18,17 @@ function loadDownload() {
     CONTAINER.appendChild(createText("FullBowody's addons scripts", null, "subtitle"));
     CONTAINER.appendChild(
         createElement("div", null, "align-x obj-container", null, null, 
-            createElement("div", null, "align-y item-container", null, dl_python,
+            createElement("div", "dl_python", "align-y item-container", null, dl_python,
                 createIcon("./resources/Python.png", null, "item-icon"),
                 createText("Python", null, "item-text")
             ),
-            createElement("div", null, "align-y item-container", null, dl_csharp,
+            createElement("div", "dl_csharp", "align-y item-container", null, dl_csharp,
                 createIcon("./resources/Csharp.png", null, "item-icon"),
                 createText("C#", null, "item-text")
             ),
-            createElement("div", null, "align-y item-container", null, dl_cpp,
+            createElement("div", "dl_cpp", "align-y item-container", null, dl_cpp,
                 createIcon("./resources/Cpp.png", null, "item-icon"),
-                createText("C/C++", null, "item-text")
+                createText("C++", null, "item-text")
             )
         )
     );
@@ -46,25 +46,29 @@ function loadDownload() {
 
 function dl_windows()
 {
-    alert("Sorry the windows executable is not available yet");
+    createInfo("Sorry, this binary is not available yet.");
+    // downloadFile("./downloads/FullBowody_win64.zip");
 }
 
 function dl_linux()
 {
-    alert("Sorry, the linux executable is not available yet");
+    createInfo("Sorry, this binary is not available yet.");
+    // downloadFile("./downloads/FullBowody_linux64.zip");
 }
 
 function dl_python()
 {
-    alert("Sorry, the Python addon is not available yet");
+    downloadFile("./downloads/FullBowody.py");
 }
 
 function dl_csharp()
 {
-    alert("Sorry, the C# addon is not available yet");
+    createInfo("Sorry, this addon is not available yet.");
+    // downloadFile("./downloads/FullBowody.cs");
 }
 
 function dl_cpp()
 {
-    alert("Sorry, the C/C++ addon is not available yet");
+    createInfo("Sorry, this addon is not available yet.");
+    // downloadFile("./downloads/FullBowody.hpp");
 }
